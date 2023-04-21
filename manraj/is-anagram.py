bd8812a0789f8d64ce77dfd7270d6560069974d5
Manraj <manraj836@gmail.com>
2023-04-21 12:01:25 -0400

    Update is-anagram.py

--- manraj/is-anagram.py
+++ manraj/is-anagram.py
@@ -1,24 +1,17 @@
+from collections import defaultdict
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if (len(s) != len(t)):
             return False
 
-        s_dict = dict()
-        t_dict = dict()
+        s_dict = defaultdict(lambda: 0)
+        t_dict = defaultdict(lambda: 0)
 
         for c in s:
-            if (c in s_dict):
-                s_dict[c] = s_dict[c] + 1
-            else:
-                s_dict[c] = 1
-        
+            s_dict[c] = s_dict[c] + 1
+
         for c in t:
-            if (c in t_dict):
-                t_dict[c] = t_dict[c] + 1
-            else:
-                t_dict[c] = 1
+            t_dict[c] = t_dict[c] + 1
         
-        if (s_dict == t_dict):
-            return True
-        else:
-            return False
+        return s_dict == t_dict
