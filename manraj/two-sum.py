5bc94e68d8ef3ef16adfa58aa70cf74e4abb4461
Manraj <manraj836@gmail.com>
2023-04-22 13:06:11 -0400

    Update two-sum.py
    Defining a dict as {} instead of dict() is slightly faster because I'm not using a function call. Also dropped the else because I'm returning in the if statement anyway which apparently sped things up but it could just be leetcode being leetcode.

--- manraj/two-sum.py
+++ manraj/two-sum.py
@@ -1,8 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        values = dict()
+        values = {}
         for i,j in enumerate(nums):
             if target - j in values:
                 return (values[target - j], i)
-            else:
-                values[j] = i
+            values[j] = i
