62b0f52cf9e74d7db2563519915553c39dc1af45
Brady-Malott <55215446+Brady-Malott@users.noreply.github.com>
2023-04-23 01:16:17 -0400

    Create valid_palindrome.java

---
+++ brady/valid_palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+  
+    // O(n) time, O(1) space
+    public boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left <= right) {
+
+            while (left <= right && !Character.isLetterOrDigit(s.charAt(left))) ++left;
+            while (left <= right && !Character.isLetterOrDigit(s.charAt(right))) --right;
+
+            if (left > right) return true;
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+
+            ++left;
+            --right;
+        }
+
+        return true;
+    }
+}
