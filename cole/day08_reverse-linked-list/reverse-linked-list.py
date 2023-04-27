4febcaadf212d35394ac9f911b2ac0b1b7f795c8
Cole <colefuerth@gmail.com>
2023-04-26 23:37:49 -0400

    day 8 linked list reversal

---
+++ cole/day08_reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,11 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        result = None
+        while head:
+            head.next, result, head = result, head, head.next
+        return result
