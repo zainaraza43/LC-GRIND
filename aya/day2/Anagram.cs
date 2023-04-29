73e45abeae55e675ec40b063388789eda491f9e2
aya-m1 <83889617+ayam02@users.noreply.github.com>
2023-04-29 18:07:21 -0400

    day 2 - anagram

---
+++ aya/day2/Anagram.cs
@@ -0,0 +1,16 @@
+﻿namespace Anagram
+{
+    public class checkAnagram
+    {
+        public static bool isAnagram(string s, string t)
+        {
+            var s1 = string.Concat(s.OrderByDescending(c => c));
+            var t1 = string.Concat(t.OrderByDescending(c => c));
+            if (string.Compare(s1, t1) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ aya/day2/test.cs
@@ -0,0 +1,13 @@
+using Anagram;
+namespace Test
+{
+    public class testClass
+    {
+        public static void Main()
+        {
+            Console.WriteLine(checkAnagram.isAnagram("anagram", "nagaram"));
+            Console.WriteLine(checkAnagram.isAnagram("car", "rat"));
+
+        }
+    }
+}
