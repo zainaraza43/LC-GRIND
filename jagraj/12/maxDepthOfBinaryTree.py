28397adb8052c80bad96ae16202557d20d047659
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-05-01 02:12:45 -0400

    More goated solution :goat:

--- jagraj/12/maxDepthOfBinaryTree.py
+++ jagraj/12/maxDepthOfBinaryTree.py
@@ -10,6 +10,4 @@ class TreeNode:
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        return 0 if root is None else 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
