ab509129243ec34a6b4eb47ba3c2a1aa97f24193
Jagraj Aulakh <aulakhjagraj@gmail.com>
2023-05-03 23:45:20 -0400

    Day 15

---
+++ jagraj/15/sameTree.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is not None and q is not None and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is None and q is None
