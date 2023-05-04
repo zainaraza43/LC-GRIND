ab156f4acb563f1ad3ee4b64a55329ab6e40e12e
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2023-05-03 21:42:20 -0400

    Optimize Day 2 LC Grind

--- christine/2-valid-anagram.java
+++ christine/2-valid-anagram.java
@@ -1,29 +1,22 @@
-import java.util.HashMap;
-import java.util.Map.Entry;
+import java.util.Arrays;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
         int slen = s.length();
         int tlen = t.length();
-        HashMap<Character, Integer> schar = new HashMap<Character, Integer>();
-        int frequency;
+        int[] freqDiff = new int[26];
+        int[] eqFreq = new int[26]; //java inits array to 0
 
-        for(int i = 0; i < slen; i++){
-            frequency = schar.get(s.charAt(i)) == null ? 0 : schar.get(s.charAt(i));
-            schar.put(s.charAt(i), frequency + 1); //find the frequency of the characters
-        }
-
-        for(int i = 0; i < tlen; i++){
-            frequency = schar.get(t.charAt(i)) == null ? 0 : schar.get(t.charAt(i));
-            schar.put(t.charAt(i), frequency - 1); //check if the frequency matches
-        }
-
-        for (Entry<Character, Integer> e : schar.entrySet()) {
-            if (e.getValue() != 0){
-                return false;
+        if (slen == tlen){//only works when slen = tlen, so no missing characters to check
+            for(int i = 0; i < slen; i++){
+                //if the frequency of the character is equal, then the element at the index is 0
+                freqDiff[s.charAt(i) - 'a']++; //s and t are lowercased strings
+                freqDiff[t.charAt(i) - 'a']--;
             }
-        }
 
-        return true;
+            return Arrays.equals(freqDiff, eqFreq);
+        }
+        
+        return false;
     }
 }
