0acf478a5cca0ce9d70993eb5995a8c1d4f27254
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2023-05-03 21:58:57 -0400

    Optimize Day 3 LC Grind

--- christine/3-two-sum.java
+++ christine/3-two-sum.java
@@ -1,14 +1,18 @@
+import java.util.HashMap;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int [] index = new int[2];
-        for(int i = 0; i < nums.length; i++){
-           for(int j = i+1; j < nums.length; j++){
-               if (nums[i]+nums[j] == target){
-                   index[0] = i;
-                   index[1] = j;
-               }
-           }
-       } 
-       return index;
+        HashMap<Integer, Integer> val = new HashMap<Integer, Integer>(); //value, index
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if (val.containsKey(comp)){
+                return new int [] {val.get(comp), i}; //the complement is the earlier / smaller index
+            }
+            else{
+                val.put(nums[i], i); //add all scanned elements
+            }
+        }
+
+        return new int [] {-1, -1}; //one valid answer exists, so won't reach here in theory
     }
 }
