348078299ce0838bb97f0ed127c676ef7f3add93
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2023-05-03 21:51:04 -0400

    Optimize Day 1 syntax

--- christine/1-contains-duplicate.java
+++ christine/1-contains-duplicate.java
@@ -3,8 +3,7 @@ import java.util.HashSet;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> values = new HashSet<Integer>();
-        int len = nums.length;
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < nums.length; i++){
             if(!values.contains(nums[i])){ //the int at nums[i] is unique
                 values.add(nums[i]);
             }
