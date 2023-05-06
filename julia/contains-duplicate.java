3c3bb342be16004708f4d69dda0cfba672db827f
Julia Garant <jgarant10@gmail.com>
2023-05-06 14:56:12 -0400

    Trying to amend

---
+++ julia/contains-duplicate.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        // int arraySize = nums.length;
+        // int dupe;
+        // // start at [0], then check [1]
+        // for(int i = 0; i < arraySize; i++){
+        //   dupe = nums[i];
+        //     // check [0] against [1],[2],[...]
+        //     for(int j = i+1; j < arraySize; j++){
+        //         if(dupe == nums[j]){
+        //           return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        /*
+         * This solution takes too long for larger test cases however it does function properly. 
+         * It passes 70/71 test cases then times out for the alloted time given.
+         */
+    }
+}
