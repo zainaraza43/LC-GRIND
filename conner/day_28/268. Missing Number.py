f3502b2cab651bc07b48c72b6860ad42fac9e73d
connersax <me@connersax.ca>
2023-05-19 21:41:56 -0400

    letsa try this again

---
+++ conner/day_28/268. Missing Number.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums) + 1):
+            if i not in nums:
+                return i
+            
+        return -1
